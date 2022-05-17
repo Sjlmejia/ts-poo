78003512a61fe57c9c7e679679fdafaaada93be5
Jorge Mejia <jorgemejia2304@gmail.com>
2022-05-17 10:44:01 -0500

    promises

---
+++ src/14-promises.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+(async() =>{
+  function delay(time:number=1000) {
+    const promise = new Promise<boolean>((resolve) => {
+      setTimeout(() => resolve(true), time);
+    });
+    return promise;
+  }
+
+  function getProducts() {
+    const promise = axios.get('https://api.escuelajs.co/api/v1/products');
+    return promise;
+  }
+
+  async function getProductsAsync() {
+    const rta = await axios.get('https://api.escuelajs.co/api/v1/products');
+    return rta.data;
+  }
+
+  const rta = await delay(3000);
+  console.log(rta);
+
+  const products = await getProducts();
+  console.log(products.data);
+
+  const productsV2 = await getProductsAsync();
+  console.log(productsV2);
+})();
